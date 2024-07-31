32f2f1788f00b7068c660a7e22f32aca99e357ec
FernandaDwi <140361266+FernandaDwi@users.noreply.github.com>
2024-07-31 12:20:54 +0700

    vidio 5
    lanjutan

--- Counter/app/src/main/java/com/example/counter/MainActivity.java
+++ Counter/app/src/main/java/com/example/counter/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.counter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHasil;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        load();
+    }
+
+    public void load()
+    {
+        tvHasil= findViewById(R.id.tvHasil);
+    }
+
+    public void btnUp(View view)
+    {
+        counter++;
+        tvHasil.setText(counter+"");
+    }
+
+    public void btnDown(View view)
+    {
+        counter--;
+        tvHasil.setText(counter+"");
     }
 }
